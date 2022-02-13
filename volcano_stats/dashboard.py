6d3346ac5c535849bc6d3ebb54fc422aadacd5af
Longfei Chen <Longfei.chen.18@ucl.ac.uk>
2022-02-13 00:07:12 +0000

    update new dataset

--- volcano_stats/dashboard.py
+++ volcano_stats/dashboard.py
@@ -9,23 +9,49 @@ import pandas as pd
 import plotly.express as px
 from dash import dcc
 from dash import html
+import plotly.graph_objs as go
 
 external_stylesheets = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, 
+    external_stylesheets=external_stylesheets)
+
+data = "Test/volcano_stats/data/Geo_Eruption_Results.xlsx"
+df = pd.read_excel(data)
+
+print(df.info())
+
+fig = go.Figure(data=go.Choropleth(
+    locations = df['Code'],
+    z = df['VEI'],
+    text = df['Country'],
+    colorscale = 'Blues',
+    autocolorscale=False,
+    reversescale=True,
+    marker_line_color='darkgray',
+    marker_line_width=0.5,
+    #colorbar_tickprefix = '$',
+    colorbar_title = 'VEI',
+))
+
 
 # assume you have a "long-form" data frame see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
+"""df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.bar(df, 
+    x="Fruit", 
+    y="Amount",
+    color="City", 
+    barmode="group"
+)"""
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
-
+    
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
@@ -35,6 +61,7 @@ app.layout = html.Div(children=[
         figure=fig
     )
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
