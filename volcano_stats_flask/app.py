575cefbb5c1d97e38a67a7347d33d183637fabce
Longfei Chen <Longfei.chen.18@ucl.ac.uk>
2022-02-25 22:56:40 +0000

    update directory

---
+++ volcano_stats_flask/app.py
@@ -0,0 +1,12 @@
+from volcano_stats_flask import create_app
+
+app = create_app()
+
+
+@app.route('/')
+def index():
+    return 'This is the home page for my_flask_app'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
