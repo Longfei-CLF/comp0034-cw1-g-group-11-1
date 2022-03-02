84b2863a8d0a959f048ca589fcba625afb772b35
Longfei Chen <Longfei.chen.18@ucl.ac.uk>
2022-03-02 13:09:45 +0000

    Jinja2 templates

--- volcano_stats_flask/app.py
+++ volcano_stats_flask/app.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from volcano_stats_flask import create_app
 from volcano_stats_flask.config import DevelopmentConfig
+from flask import render_template
 
 app = create_app(DevelopmentConfig)
 
-
 @app.route('/')
 def index():
-    return 'This is the home page for my_flask_app'
-
+    return render_template('index.html', title="Home")
 
 if __name__ == '__main__':
     app.run(debug=True)
